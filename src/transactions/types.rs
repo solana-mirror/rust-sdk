6fbd083060c4895e39498630380d5f376123746a
setPedro <pedross4789@gmail.com>
2025-01-25 19:56:49 +0100

    SM-21: Add docs

--- src/transactions/types.rs
+++ src/transactions/types.rs
@@ -3,14 +3,12 @@ use std::collections::HashMap;
 
 use crate::types::FormattedAmount;
 
-/// Stores the pre and post balances of a transaction
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct BalanceChange {
     pub pre: FormattedAmount,
     pub post: FormattedAmount,
 }
 
-/// Represents a parsed transaction
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct ParsedTransaction {
     #[serde(rename = "blockTime")]
